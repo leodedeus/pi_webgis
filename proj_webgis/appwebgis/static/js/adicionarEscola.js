04395bec922e255465e0e865561e917a2a6b55e1
leodedeus <leorodriguesdedeus@gmail.com>
2023-12-04 12:58:39 -0300

    ajuste funcionalidade criar escola

--- proj_webgis/appwebgis/static/js/adicionarEscola.js
+++ proj_webgis/appwebgis/static/js/adicionarEscola.js
@@ -20,33 +20,41 @@ document.addEventListener('DOMContentLoaded', function () {
                 const lng = event.latlng.lng;
 
                 // Abre um formulário para preencher as demais informações
-                const nomeEscola = prompt('Digite o nome da escola:');
+                let nomeEscola;
 
-                // Envia as coordenadas, nome e outras informações para o servidor
-                fetch('/adicionar_escola', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept':'application/json'
-                    },
-                    body: JSON.stringify({
-                        nome: nomeEscola,
-                        latitude: lat,
-                        longitude: lng,
-                    }),
-                })
-                .then(response => response.json())
-                .then(data => {
-                    // Exibe um alerta com a resposta do servidor
-                    console.log('Data recebida:', data);
-                    alert(data.message);
+                do {
+                    // Pede o nome da escola até que seja fornecido ou o usuário clique em "Cancelar"
+                    nomeEscola = prompt('Digite o nome da escola:');
+                } while (nomeEscola !== null && nomeEscola.trim() === '');  // Repete até que seja fornecido um nome não vazio
 
-                    // Recarrega a página para exibir a nova escola
-                    location.reload();
-                })
-                .catch(error => {
-                    console.error('Erro ao adicionar escola:', error);
-                });
+                // Verifica se o nome da escola é válido
+                if (nomeEscola !== null) {
+                    // Envia as coordenadas, nome e outras informações para o servidor
+                    fetch('/adicionar_escola', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            nome: nomeEscola,
+                            latitude: lat,
+                            longitude: lng,
+                        }),
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        // Exibe um alerta com a resposta do servidor
+                        console.log('Data recebida:', data);
+                        alert(data.message);
+
+                        // Recarrega a página para exibir a nova escola
+                        location.reload();
+                    })
+                    .catch(error => {
+                        console.error('Erro ao adicionar escola:', error);
+                    });
+                }
             };
 
             // Adiciona o ouvinte de evento ao mapa
@@ -54,4 +62,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
